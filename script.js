c5ed65d22bb5ba9e77ce637a5f61f59d8ec48add
Amrut <113540993+Amruth-07@users.noreply.github.com>
2026-01-31 05:20:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -317,4 +317,13 @@ function copyCode() {
 
 function toggleTheme(){
   document.body.classList.toggle("light-mode");
+
+  const icon = document.querySelector(".theme-toggle i");
+  if(document.body.classList.contains("light-mode")){
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }else{
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  }
 }
